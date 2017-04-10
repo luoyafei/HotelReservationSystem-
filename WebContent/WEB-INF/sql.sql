0c8db0a12da54ba159b4a3e1454c4ea64c96cdb6
luoyafei <luoyafei.newinstance@foxmail.com>
2017-04-10 16:01:54 +0800

    完成数据库表的设计

---
+++ WebContent/WEB-INF/sql.sql
@@ -0,0 +1,46 @@
+create database Hotel;
+use Hote;
+create table t_hotel (
+	hotelId varchar(32) primary key, 
+	hotelName varchar(32),
+	hotelAddress varchar(255),
+	inOUtTimeDesc varchar(255),
+	hotelTel varchar(32),
+	internetService varchar(255),
+	openTime varchar(255),
+	hotelDevice varchar(255),
+	hotelService varchar(255),
+	hotelBrief varchar(255),
+	browseMap varchar(255),
+	createDate datetime default current_timestamp
+) engine=innoDB default charset=utf8;
+
+create table t_house (
+	houseId varchar(32) primary key,
+	belongHotelId varchar(32),
+	browseMap varchar(255),
+	houseInfo varchar(255),
+	cheapService varchar(255),
+	price varchar(11),
+	isOrder varchar(2),
+	createDate datetime default current_timestamp,
+	constraint foreign key (belongHotelId) references t_hotel (hotelId)
+) engine=innoDB default charset=utf8;
+
+create table t_subscribe (
+	subscribeId varchar(32) primary key,
+	hotelId varchar(32),
+	houseId varchar(32),
+	isUnsubscribe varchar(2),
+	subscribeTime datatime,
+	unsubscribeTime datetime,
+	constraint foreign key (hotelId) references t_hotel (hotelId),
+	constraint foreign key (houseId) references t_house (houseId)
+) engine=innoDB default charset=utf8;
+
+create table t_user (
+	userId varchar(32) primary key,
+	userTel varchar(32),
+	userPassword varchar(32)
+) engine=innoDB default charset=utf8;
+
