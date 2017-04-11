9b035592537c1c61e547b6dc703602dee0723721
luoyafei <luoyafei.newinstance@foxmail.com>
2017-04-11 21:07:53 +0800

    修改底层sql语句

--- WebContent/WEB-INF/sql.sql
+++ WebContent/WEB-INF/sql.sql
@@ -1,5 +1,13 @@
 create database Hotel;
 use Hote;
+
+create table t_admin (
+	adminId varchar(32) primary key,
+	adminNum varchar(32),
+	adminPwd varchar(32),
+	recentLogin datetime
+) engine=innoDB default charset=utf8;
+
 create table t_hotel (
 	hotelId varchar(32) primary key, 
 	hotelName varchar(32),
@@ -29,11 +37,13 @@ create table t_house (
 
 create table t_subscribe (
 	subscribeId varchar(32) primary key,
+	userId varchar(32),
 	hotelId varchar(32),
 	houseId varchar(32),
 	isUnsubscribe varchar(2),
-	subscribeTime datatime,
+	subscribeTime datetime,
 	unsubscribeTime datetime,
+	constraint foreign key (userId) references t_user (userId),
 	constraint foreign key (hotelId) references t_hotel (hotelId),
 	constraint foreign key (houseId) references t_house (houseId)
 ) engine=innoDB default charset=utf8;
@@ -41,6 +51,7 @@ create table t_subscribe (
 create table t_user (
 	userId varchar(32) primary key,
 	userTel varchar(32),
-	userPassword varchar(32)
+	userPassword varchar(32),
+	userIntegral varchar(11)
 ) engine=innoDB default charset=utf8;
 
